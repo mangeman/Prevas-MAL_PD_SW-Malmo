baa3572cd5250c455e541e5dbb4dc45d284812d3
htydesjo <henrik.tydesjo@fastmail.net>
2013-12-09 11:09:23 +0100

    Use of ultrasonic sensor. Improvements on set_speed/turn logic.

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,9 @@
 #define ARDUINO_MAIN
 #include "Arduino.h"
 
-int speed = 0; // [-255..255]
+byte speed = 0; // [0..255]
+
+float distance = 100;
 
 #define pwmPinRA1 13
 #define pwmPinRA2 12
@@ -12,26 +14,32 @@ int speed = 0; // [-255..255]
 #define pwmPinLB1 2
 #define pwmPinLB2 3
 
-void set_speed(int spe);
+#define pinUltraEcho 51
+
+void set_speed(byte spe);
 void stop(void);
-void turn_left(void);
-void turn_left2(void);
-void turn_right(void);
-void turn_right2(void);
+void stop_left(void);
+void stop_right(void);
+void forward(void);
+void backward(void);
+void left_forward(void);
+void left_backward(void);
+void right_forward(void);
+void right_backward(void);
+void turn_left_forward(void);
+void turn_left_backward(void);
+void turn_right_forward(void);
+void turn_right_backward(void);
+
+void measure_distance(void);
 
 //____________________________________________________________
-int main() 
+int main()
 {
     init();
 
     delay(1);
 
-    //    unsigned short pin1 = 12;
-    unsigned short pin2 = 51;
-    int sensorPin = A0;
-    int sensorValue = 0;
-    //    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
     pinMode(pwmPinRA1, OUTPUT);
     pinMode(pwmPinRA2, OUTPUT);
     pinMode(pwmPinRB1, OUTPUT);
@@ -40,8 +48,8 @@ int main()
     pinMode(pwmPinLA2, OUTPUT);
     pinMode(pwmPinLB1, OUTPUT);
     pinMode(pwmPinLB2, OUTPUT);
-    //    digitalWrite(pin1, HIGH);
-    digitalWrite(pin2, HIGH);
+    
+    pinMode(pinUltraEcho, INPUT);
 
 
 #if defined(USBCON)
@@ -51,209 +59,131 @@ int main()
     // initialize serial:
     Serial.begin(115200);
 
-    int delay1 = 3000;
+    //int delay1 = 3000;
 
-    //    for (;;) 
+    set_speed(255);
+
+    for (;;)
         {
-            static int bPP = 0;
- 
-            //            sensorValue = analogRead(sensorPin);
+            measure_distance();
 
-            stop();
-
-            set_speed(255);
-            delay(delay1);
-
-            turn_right2();
-            delay(delay1);
-
-            set_speed(127);
-            delay(delay1);
-
-            set_speed(255);
-            turn_left2();
-            delay(delay1);
-
-            set_speed(-255);
-            delay(delay1);
-
-            turn_right2();
-            delay(delay1);
-
-            set_speed(-127);
-            delay(delay1);
-
-            set_speed(-255);
-            turn_left2();
-            delay(delay1);
-
-            stop();
-            
-
-//!!!            if (bPP%100 == 0)
-//!!!                {
-//!!!                    Serial.print("Hello Wold: \r\n");
-//!!!                }
-
-            ++bPP;
+            if (distance < 20)
+            {
+                turn_right_backward();
+                for (int i=0; i<45; ++i)
+                {
+                  measure_distance();
+                  delay(10);
+                }
+            }
+            else
+            {
+                forward();
+            }
         }
     
     return 0;
 }
-//____________________________________________________________
-void stop(void)
+//__________________________________________________________________
+void measure_distance(void)
 {
-    analogWrite(pwmPinRA1, 0);
-    analogWrite(pwmPinRA2, 0);
-    analogWrite(pwmPinRB1, 0);
-    analogWrite(pwmPinRB2, 0);
+  // PW DIGITAL IN
+  long raw = pulseIn(pinUltraEcho, HIGH);
+  distance = 0.75*distance + 0.25*(raw*0.017279); // 2.54 / 147 [cm]
+}
+//____________________________________________________________
+void stop_left(void)
+{
     analogWrite(pwmPinLA1, 0);
     analogWrite(pwmPinLA2, 0);
     analogWrite(pwmPinLB1, 0);
     analogWrite(pwmPinLB2, 0);
 }
-//____________________________________________________________
-void set_speed(int spe)
+void stop_right(void)
 {
-    speed = spe;
-
-    // speed must be [-255..255]
-    if (speed < 0)
-        {
-            analogWrite(pwmPinRA1, 0);
-            analogWrite(pwmPinRA2, -speed);
-            analogWrite(pwmPinRB1, 0);
-            analogWrite(pwmPinRB2, -speed);
-
-            analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, -speed);
-            analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, -speed);
-        }
-    else
-        {
-            analogWrite(pwmPinRA1, speed);
-            analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, speed);
-            analogWrite(pwmPinRB2, 0);
-
-            analogWrite(pwmPinLA1, speed);
-            analogWrite(pwmPinLA2, 0);
-            analogWrite(pwmPinLB1, speed);
-            analogWrite(pwmPinLB2, 0);
-        }    
+    analogWrite(pwmPinRA1, 0);
+    analogWrite(pwmPinRA2, 0);
+    analogWrite(pwmPinRB1, 0);
+    analogWrite(pwmPinRB2, 0);
 }
 //____________________________________________________________
-void turn_left(void)
+void stop(void)
 {
-    if (speed < 0)
-        {
-            analogWrite(pwmPinRA1, 0);
-            analogWrite(pwmPinRA2, -speed);
-            analogWrite(pwmPinRB1, 0);
-            analogWrite(pwmPinRB2, -speed);
-
-            analogWrite(pwmPinLA1, -speed);
-            analogWrite(pwmPinLA2, 0);
-            analogWrite(pwmPinLB1, -speed);
-            analogWrite(pwmPinLB2, 0);
-        }
-    else
-        {
-            analogWrite(pwmPinRA1, speed);
-            analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, speed);
-            analogWrite(pwmPinRB2, 0);
-
-            analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, speed);
-            analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, speed);
-        }    
+    stop_left();
+    stop_right();
 }
 //____________________________________________________________
-void turn_left2(void)
+void set_speed(byte spe)
 {
-    if (speed < 0)
-        {
-            analogWrite(pwmPinRA1, 0);
-            analogWrite(pwmPinRA2, -speed);
-            analogWrite(pwmPinRB1, 0);
-            analogWrite(pwmPinRB2, -speed);
-
-            analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, 0);
-            analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, 0);
-        }
-    else
-        {
-            analogWrite(pwmPinRA1, speed);
-            analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, speed);
-            analogWrite(pwmPinRB2, 0);
-
-            analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, 0);
-            analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, 0);
-        }    
+  speed = spe;
 }
 //____________________________________________________________
-void turn_right(void)
+void forward()
 {
-    if (speed < 0)
-        {
-            analogWrite(pwmPinRA1, -speed);
-            analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, -speed);
-            analogWrite(pwmPinRB2, 0);
-
-            analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, -speed);
-            analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, -speed);
-        }
-    else
-        {
+  right_forward();
+  left_forward();
+}
+//____________________________________________________________
+void right_backward()
+{
             analogWrite(pwmPinRA1, 0);
             analogWrite(pwmPinRA2, speed);
             analogWrite(pwmPinRB1, 0);
             analogWrite(pwmPinRB2, speed);
-
-            analogWrite(pwmPinLA1, speed);
-            analogWrite(pwmPinLA2, 0);
-            analogWrite(pwmPinLB1, speed);
-            analogWrite(pwmPinLB2, 0);
-        }
 }
 //____________________________________________________________
-void turn_right2(void)
+void left_backward()
 {
-    if (speed < 0)
-        {
-            analogWrite(pwmPinRA1, 0);
-            analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, 0);
-            analogWrite(pwmPinRB2, 0);
-
             analogWrite(pwmPinLA1, 0);
-            analogWrite(pwmPinLA2, -speed);
+            analogWrite(pwmPinLA2, speed);
             analogWrite(pwmPinLB1, 0);
-            analogWrite(pwmPinLB2, -speed);
-        }
-    else
-        {
-            analogWrite(pwmPinRA1, 0);
+            analogWrite(pwmPinLB2, speed);
+}
+//____________________________________________________________
+void backward()
+{
+  right_backward();
+  left_backward();
+}
+//____________________________________________________________
+void right_forward()
+{
+            analogWrite(pwmPinRA1, speed);
             analogWrite(pwmPinRA2, 0);
-            analogWrite(pwmPinRB1, 0);
+            analogWrite(pwmPinRB1, speed);
             analogWrite(pwmPinRB2, 0);
-
+}
+//____________________________________________________________
+void left_forward()
+{
             analogWrite(pwmPinLA1, speed);
             analogWrite(pwmPinLA2, 0);
             analogWrite(pwmPinLB1, speed);
             analogWrite(pwmPinLB2, 0);
-        }
+}
+//____________________________________________________________
+void turn_left_backward(void)
+{
+  stop_left();
+  right_backward();  
+}
+//____________________________________________________________
+void turn_left_forward(void)
+{
+  stop_left();
+  right_forward();
+}
+//____________________________________________________________
+void turn_right_backward(void)
+{
+  stop_right();
+  left_backward();
+}
+//____________________________________________________________
+void turn_right_forward(void)
+{
+  stop_right();
+  left_forward();
 }
 
 
@@ -262,23 +192,23 @@ void turn_right2(void)
 
 
 
-//    for (;;) 
-//        {
-//            Serial.print("Hello Wold\r\n");
-//            static unsigned char bFlip = 0;
-//            ++bFlip;
-//            if (bFlip%2==0)
-//                {
-//                    digitalWrite(pin1, HIGH);
-//                    digitalWrite(pin2, LOW);
-//                    analogWrite(pwmPin, 127);
-//                }
-//            else
-//                {
-//                    digitalWrite(pin1, LOW);
-//                    digitalWrite(pin2, HIGH);
-//                    analogWrite(pwmPin, 255);
-//                }
-//            delay(5000);
-//            if (serialEventRun) serialEventRun();
-//        }
+// for (;;)
+// {
+// Serial.print("Hello Wold\r\n");
+// static unsigned char bFlip = 0;
+// ++bFlip;
+// if (bFlip%2==0)
+// {
+// digitalWrite(pin1, HIGH);
+// digitalWrite(pin2, LOW);
+// analogWrite(pwmPin, 127);
+// }
+// else
+// {
+// digitalWrite(pin1, LOW);
+// digitalWrite(pin2, HIGH);
+// analogWrite(pwmPin, 255);
+// }
+// delay(5000);
+// if (serialEventRun) serialEventRun();
+// }
